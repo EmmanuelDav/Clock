8d4b8870bcadb35fec3fe571ad144b003595825d
Kura <kuradavis987@gmail.com>
2020-08-10 04:12:20 +0100

    updates on timer

--- app/src/main/java/com/example/alarm/Fragments/TimerFragment.kt
+++ app/src/main/java/com/example/alarm/Fragments/TimerFragment.kt
@@ -1,21 +1,22 @@
 package com.example.alarm.Fragments
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import androidx.fragment.app.Fragment
 import com.example.alarm.R
 import kotlinx.android.synthetic.main.fragment_timer.*
+import java.util.*
 
 class TimerFragment : Fragment() {
     companion object {
-        var sStartTimer: Long = 60_000
+        var sStartTimer: Long = 60_000 // one min
         var sTimerLeft: Long = sStartTimer
         var sRunning: Boolean = false
-
     }
 
     override fun onCreateView(
@@ -47,18 +48,46 @@ class TimerFragment : Fragment() {
     private fun startTimer() {
         if (!sRunning) {
             timerProgress.max = sStartTimer.toInt()
-            val timer = object : CountDownTimer(sTimerLeft,1_000) {
+            timerProgress.progress = sTimerLeft.toInt()
+            val timer = object : CountDownTimer(sTimerLeft, 1_000) {
                 override fun onFinish() {
-
+                    textTimer.text = "Finished"
                 }
 
                 override fun onTick(millisUntilFinished: Long) {
-
+                    var sObjectAnimator: ObjectAnimator = ObjectAnimator
+                        .ofInt(
+                            timerProgress,
+                            "ProgressBar",
+                            timerProgress.progress,
+                            millisUntilFinished.toInt()
+                        )
+                    sObjectAnimator.duration = 300
+                    sObjectAnimator.interpolator = LinearInterpolator()
+                    sObjectAnimator.start()
+                    timerProgress.progress = millisUntilFinished.toInt()
+                    sTimerLeft = millisUntilFinished
+                    updateTimerText()
                 }
             }
+            timer.start()
             sRunning = true
         }
+
     }
 
+    private fun updateTimerText() {
+        val seconds = sTimerLeft.toInt() / 1000 % 60 //seconds
+        val minutes = sTimerLeft.toInt() / 1000 / 60 % 60 // min
+        val hours = sTimerLeft.toInt() / 1000 / 60 / 60 % 24 // hours
+        val timeLeftFormatted = String.format(
+            Locale.getDefault(),
+            "%02d:%02d:%02d",
+            hours,
+            minutes,
+            seconds
+        )
+        textTimer.text = timeLeftFormatted
+    }
 
 }
